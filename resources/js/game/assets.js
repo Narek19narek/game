8e4141d0c192e4ed9e3e0fd3707330e0152f4fc9
Narek Mamikonyan <mamikonyan95@gmail.com>
2019-10-22 18:57:51 +0400

    background ok

--- resources/js/game/assets.js
+++ resources/js/game/assets.js
@@ -1,23 +1,23 @@
-// const ASSET_NAMES = [
-//   'color-1.svg',
-// ];
-//
-// const assets = {};
-//
-// const downloadPromise = Promise.all(ASSET_NAMES.map(downloadAsset));
-//
-// function downloadAsset(assetName) {
-//   return new Promise(resolve => {
-//     const asset = new Image();
-//     asset.onload = () => {
-//       console.log(`Downloaded ${assetName}`);
-//       assets[assetName] = asset;
-//       resolve();
-//     };
-//     asset.src = `/images/skins/${assetName}`;
-//   });
-// }
-//
-// export const downloadAssets = () => downloadPromise;
-//
-// export const getAsset = assetName => assets[assetName];
+const ASSET_NAMES = [
+  'bg.svg',
+];
+
+const assets = {};
+
+const downloadPromise = Promise.all(ASSET_NAMES.map(downloadAsset));
+
+function downloadAsset(assetName) {
+  return new Promise(resolve => {
+    const asset = new Image();
+    asset.onload = () => {
+      console.log(`Downloaded ${assetName}`);
+      assets[assetName] = asset;
+      resolve();
+    };
+    asset.src = `/images/${assetName}`;
+  });
+}
+
+export const downloadAssets = () => downloadPromise;
+
+export const getAsset = assetName => assets[assetName];
--- resources/js/game/render.js
+++ resources/js/game/render.js
@@ -25,61 +25,62 @@ if (document.getElementById('user_info')) {
 setCanvasDimensions();
 
 function setCanvasDimensions() {
-  // On small screens (e.g. phones), we want to "zoom out" so players can still see at least
-  // 800 in-game units of width.
-  const scaleRatio = Math.max(1, 800 / window.innerWidth);
-  canvas.width = scaleRatio * window.innerWidth;
-  canvas.height = scaleRatio * window.innerHeight;
+    // On small screens (e.g. phones), we want to "zoom out" so players can still see at least
+    // 800 in-game units of width.
+    const scaleRatio = Math.max(1, 800 / window.innerWidth);
+    canvas.width = scaleRatio * window.innerWidth;
+    canvas.height = scaleRatio * window.innerHeight;
 }
 
 function render() {
-  const { me, others } = getCurrentState();
-  if (!me) {
-    return;
-  }
+    const { me, others } = getCurrentState();
+    if (!me) {
+        return;
+    }
 
-  // Draw background
-  renderBackground(me.x, me.y);
+    // Draw background
+    renderBackground(me.x, me.y);
 
-  // Draw boundaries
-  context.beginPath();
-  context.strokeStyle = Constants.GAME_MODE.BORDER_COLOR[gameMode];
-  context.lineWidth = 5;
-  context.strokeRect(canvas.width / 2 - me.x, canvas.height / 2 - me.y, MAP_SIZE, MAP_SIZE);
+    // Draw boundaries
+    context.beginPath();
+    context.strokeStyle = Constants.GAME_MODE.BORDER_COLOR[gameMode];
+    context.lineWidth = 5;
+    context.strokeRect(canvas.width / 2 - me.x, canvas.height / 2 - me.y, MAP_SIZE, MAP_SIZE);
 
-  // Draw all players
-  renderPlayer(me, me);
-  others.forEach(renderPlayer.bind(null, me));
+    // Draw all players
+    renderPlayer(me, me);
+    others.forEach(renderPlayer.bind(null, me));
 }
 
 function renderBackground(x, y) {
 
-  const backgroundX = MAP_SIZE / 2 - x + canvas.width / 2;
-  const backgroundY = MAP_SIZE / 2 - y + canvas.height / 2;
-  // context.drawImage(getAsset('bg.png'), backgroundX, backgroundY);
-  const backgroundGradient = context.createRadialGradient(
-    backgroundX,
-    backgroundY,
-    0,
-    backgroundX,
-    backgroundY,
-    MAP_SIZE / 2,
-  );
-  backgroundGradient.addColorStop(1, Constants.GAME_MODE.BG_COLOR[gameMode]);
-  backgroundGradient.addColorStop(0, Constants.GAME_MODE.BG_COLOR[gameMode]);
-  context.fillStyle = backgroundGradient;
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  // context.clearRect(0, 0, MAP_SIZE, MAP_SIZE);
+    const backgroundX = MAP_SIZE / 2 - x + canvas.width / 2;
+    const backgroundY = MAP_SIZE / 2 - y + canvas.height / 2;
+    // context.drawImage(getAsset('bg.png'), backgroundX, backgroundY);
+    const backgroundGradient = context.createRadialGradient(
+        backgroundX,
+        backgroundY,
+        0,
+        backgroundX,
+        backgroundY,
+        MAP_SIZE / 2,
+    );
+    backgroundGradient.addColorStop(1, Constants.GAME_MODE.BG_COLOR[gameMode]);
+    backgroundGradient.addColorStop(0, Constants.GAME_MODE.BG_COLOR[gameMode]);
+    context.fillStyle = backgroundGradient;
+    context.clearRect(0, 0, MAP_SIZE, MAP_SIZE);
+    context.fillRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
+    // const length = canvas.width / 2 >
     for (let i = 0; i < 250; i++) {
-        context.moveTo(canvas.width / 2 - x + (i - 125) * 40, -y);
-        context.lineTo(2 * (MAP_SIZE + canvas.width) - x + (i - 125) * 40,2 * (MAP_SIZE + canvas.height)- y);
+        context.moveTo(-x + (i - 125) * 40, -y);
+        context.lineTo(MAP_SIZE - x + canvas.width + (i - 125) * 40,MAP_SIZE - y + canvas.width);
 
-        context.moveTo(2 * (MAP_SIZE + canvas.width) - x - i * 40, -y);
-        context.lineTo(canvas.width / 2 - x - i * 40,2 * (MAP_SIZE + canvas.height) - y);
+        context.moveTo(MAP_SIZE - x + canvas.width + (i - 125) * 40, -y);
+        context.lineTo(-x + (i - 125) * 40,MAP_SIZE - y + canvas.width);
 
-        context.moveTo(2 * (MAP_SIZE + canvas.width) + x ,-y + i * ((canvas.height + MAP_SIZE) * 43.8 / (MAP_SIZE + canvas.width)));
-        context.lineTo(- x,-y  + i * ((canvas.height + MAP_SIZE) * 43.8 / (MAP_SIZE + canvas.width)));
+        context.moveTo(-x ,-y + i * 40);
+        context.lineTo(MAP_SIZE + canvas.width,-y  + i * 40 );
     }
 
     var grad = context.createRadialGradient(backgroundX,
@@ -97,25 +98,25 @@ function renderBackground(x, y) {
 
 // Renders a ship at the given coordinates
 function renderPlayer(me, player) {
-  const { x, y } = player;
-  const canvasX = canvas.width / 2 + x - me.x;
-  const canvasY = canvas.height / 2 + y - me.y;
-  // Draw ship
-  context.save();
-  context.translate(canvasX, canvasY);
-  // if (player.rotate) {
-  //   context.rotate(player.rotate / 57 * 3);
-  // }
+    const { x, y } = player;
+    const canvasX = canvas.width / 2 + x - me.x;
+    const canvasY = canvas.height / 2 + y - me.y;
+    // Draw ship
+    context.save();
+    context.translate(canvasX, canvasY);
+    // if (player.rotate) {
+    //   context.rotate(player.rotate / 57 * 3);
+    // }
 
-  // let img;
-  // if (player.status === 2) {
-  //   img = 'square.svg';
-  // } else if (player.status === 0) {
-  //   img = 'triangle.svg';
-  // } else img = 'circle.svg';
-  // contextSquare.clearRect(0, 0, 100, 100);
-  // contextSquare.beginPath();
-  context.beginPath();
+    // let img;
+    // if (player.status === 2) {
+    //   img = 'square.svg';
+    // } else if (player.status === 0) {
+    //   img = 'triangle.svg';
+    // } else img = 'circle.svg';
+    // contextSquare.clearRect(0, 0, 100, 100);
+    // contextSquare.beginPath();
+    context.beginPath();
     if (player.status === 2) {
         context.moveTo(-25, -25);
         context.lineTo(-25, 25);
@@ -131,93 +132,92 @@ function renderPlayer(me, player) {
     } else {
         context.arc(0, 0, 25, 0, 2 * Math.PI);
     }
-  context.strokeStyle = Constants.SKINS[player.skin];
-  context.lineWidth = 4;
-  context.stroke();
-  context.restore();
+    context.strokeStyle = Constants.SKINS[player.skin];
+    context.lineWidth = 4;
+    context.stroke();
+    context.restore();
 
-  // Draw health bar
-  // context.fillRect(
-  //   canvasX - PLAYER_RADIUS,
-  //   canvasY + PLAYER_RADIUS + 20,
-  //   PLAYER_RADIUS * 2,
-  //   2,
-  // );
-  const textX = canvasX - PLAYER_RADIUS + 12;
-  let textPos = textX;
+    // Draw health bar
+    // context.fillRect(
+    //   canvasX - PLAYER_RADIUS,
+    //   canvasY + PLAYER_RADIUS + 20,
+    //   PLAYER_RADIUS * 2,
+    //   2,
+    // );
+    const textX = canvasX - PLAYER_RADIUS + 12;
+    let textPos = textX;
 
-  const getPlayer = player_data.find((obj) => {
-      return  obj.id === player.id;
-  });
-  let playerPosition = getPlayer ? getPlayer.positionId : '';
-  let playerName = player.username;
+    const getPlayer = player_data.find((obj) => {
+        return  obj.id === player.id;
+    });
+    let playerPosition = getPlayer ? getPlayer.positionId : '';
+    let playerName = player.username;
 
-  if (player !== me) {
-      if (player.hidePosition === '1') {
-          playerPosition = '';
-      }
-      if (player.hideName === '1') {
-          playerName = '';
-      }
-  }
+    if (player !== me) {
+        if (player.hidePosition === '1') {
+            playerPosition = '';
+        }
+        if (player.hideName === '1') {
+            playerName = '';
+        }
+    }
 
     if (playerPosition === 1 ) {
-      context.fillStyle = '#FFA200';
-      context.font = '25px FuturaPress';
-  } else if (playerPosition > 1) {
-      context.fillStyle = Constants.SKINS[player.skin];
-      context.font = '16px FuturaPress';
-      if (playerPosition.toString().length === 2) {
-          textPos = textX - 4;
-      } else if (playerPosition.toString().length === 3) {
-          textPos = textX - 8;
-      }
-  }
-  const textY = (player.status === 0) ? canvasY + PLAYER_RADIUS - 8 : canvasY + PLAYER_RADIUS;
-  context.fillText(playerPosition, textPos, canvasY - PLAYER_RADIUS - 18);
-  context.fillStyle = Constants.SKINS[player.skin];
-  context.font = '16px FuturaPress';
+        context.fillStyle = '#FFA200';
+        context.font = '25px FuturaPress';
+    } else if (playerPosition > 1) {
+        context.fillStyle = Constants.SKINS[player.skin];
+        context.font = '16px FuturaPress';
+        if (playerPosition.toString().length === 2) {
+            textPos = textX - 4;
+        } else if (playerPosition.toString().length === 3) {
+            textPos = textX - 8;
+        }
+    }
+    const textY = (player.status === 0) ? canvasY + PLAYER_RADIUS - 8 : canvasY + PLAYER_RADIUS;
+    context.fillText(playerPosition, textPos, canvasY - PLAYER_RADIUS - 18);
+    context.fillStyle = Constants.SKINS[player.skin];
+    context.font = '16px FuturaPress';
 
 
-  let x1;
-  if (playerName.length < 4) {
-      x1 = playerName.length * playerName.length;
-  } else {
-      switch (playerName.length) {
-          case 4: x1 = 10;
-            break;
-          case 5: x1 = 14;
-            break;
-          default: x1 = 18;
-      }
-  }
-  const text = playerName.length > 6 ? playerName.slice(0, 6) : playerName;
-  context.fillText(text, textX - x1, textY + 22);
+    let x1;
+    if (playerName.length < 4) {
+        x1 = playerName.length * playerName.length;
+    } else {
+        switch (playerName.length) {
+            case 4: x1 = 10;
+                break;
+            case 5: x1 = 14;
+                break;
+            default: x1 = 18;
+        }
+    }
+    const text = playerName.length > 6 ? playerName.slice(0, 6) : playerName;
+    context.fillText(text, textX - x1, textY + 22);
 }
 
 function renderMainMenu() {
-  const t = Date.now() / 7500;
-  const x = MAP_SIZE / 2 + 800 * Math.cos(t);
-  const y = MAP_SIZE / 2 + 800 * Math.sin(t);
-  renderBackground(x, y);
+    const t = Date.now() / 7500;
+    const x = MAP_SIZE / 2 + 800 * Math.cos(t);
+    const y = MAP_SIZE / 2 + 800 * Math.sin(t);
+    renderBackground(x, y);
 }
 
-let renderInterval = setInterval(renderMainMenu, 1000 / 60);
+let renderInterval = setInterval(renderMainMenu, 1000 / 50);
 
 // Replaces main menu rendering with game rendering.
 export function startRendering() {
-  clearInterval(renderInterval);
-  renderInterval = setInterval(render, 1000 / 60);
+    clearInterval(renderInterval);
+    renderInterval = setInterval(render, 1000 / 50);
 }
 
 // Replaces game rendering with main menu rendering.
 export function stopRendering() {
-  clearInterval(renderInterval);
-  renderInterval = setInterval(renderMainMenu, 1000 / 60);
+    clearInterval(renderInterval);
+    renderInterval = setInterval(renderMainMenu, 1000 / 50);
 }
 
 export function getInfo() {
     const { me } = getCurrentState();
     return me;
 }
-
